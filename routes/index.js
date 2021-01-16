2449cfa95e9a5dfcab262b4ee5aeb212437d766b
moyaaaaaa <danmakubi@gmail.com>
2021-01-16 23:40:06 +0900

    APIをテンプレートのままからPOSTを受ける形に書き換える

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.post('/', function(req, res, next) {
+    // requestを確認
+
+    // payloadから画像URLを抜き出す
+
+    // 画像を取得
+    // Dropbox APIにそのまま渡せるかどうか
+
+    // Dropbox APIのcreateを叩く
+    
+    
+    res.json({result: 'success'});
 });
 
 module.exports = router;
