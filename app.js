0950fafbd87db163ceecec5488282c8566dda584
moyaaaaaa <danmakubi@gmail.com>
2021-01-17 00:00:24 +0900

    bodyparseの設定を追加 リクエストパラメータをreq.bodyで取得したいため

--- app.js
+++ app.js
@@ -18,6 +18,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+
 app.use('/', indexRouter);
 
 
